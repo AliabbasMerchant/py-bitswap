08a41b88937ff0e2b778c7e363f796e281388769
Aliabbas Merchant <merchantaliabbas@gmail.com>
2019-02-16 23:19:23 +0530

    Added IPFSBlock

---
+++ bitswap/IPFSBlock.py
@@ -0,0 +1,49 @@
+import cid as py_cid
+
+
+class Block:
+    """
+    Represents an immutable block of data that is uniquely referenced with a cid.
+    """
+
+    def __init__(self, data, cid: py_cid.cid):
+        """
+        Represents an immutable block of data that is uniquely referenced with a cid.
+
+        :param data: An object supporting the buffer protocol
+        :param cid: A cid
+        """
+        try:
+            memoryview(data)
+        except TypeError:
+            raise ValueError("Data must be an object supporting the buffer protocol.")
+        if not py_cid.is_cid(str(cid)):
+            raise ValueError(f"{cid} is not a valid cid")
+        self._data = data
+        self._cid = cid
+
+    @property
+    def data(self):
+        """
+        The data of this block
+
+        :return: The data of this block
+        """
+        return self._data
+
+    @data.setter
+    def data(self, val):
+        raise RuntimeError("Cannot change the data of a block")
+
+    @property
+    def cid(self):
+        """
+        The cid of this block
+
+        :return: The cid of this block
+        """
+        return self._cid
+
+    @cid.setter
+    def cid(self, val):
+        raise RuntimeError("Cannot change the cid of a block")
