cd7d0cfd6fc42634c54b1c60a1615f2724706ed3
Aliabbas Merchant <merchantaliabbas@gmail.com>
2019-02-16 17:48:54 +0530

    Added WantList and WantList entries

---
+++ bitswap/types/wantlist/WantList.py
@@ -0,0 +1,56 @@
+from .WantListEntry import WantListEntry
+from collections import OrderedDict
+import cid as py_cid
+
+
+# TODO: What is stats
+class WantList:
+    Entry = WantListEntry  # TODO: Why ?
+
+    def __init__(self, stats):
+        self.entries = OrderedDict()
+        self._stats = stats
+
+    def __len__(self):
+        return len(self.entries)
+
+    def add(self, cid: py_cid.cid, priority: int):
+        cid_str = str(cid)
+        entry = self.entries.get(cid_str)
+        if entry is not None:
+            entry.inc()
+            entry.priority = priority  # TODO: Why ?
+        else:
+            self.entries[cid_str] = WantListEntry(cid, priority)
+            if self._stats is not None:
+                self._stats.push(None, 'WantListSize', 1)  # TODO
+
+    def remove(self, cid: py_cid.cid):
+        cid_str = str(cid)
+        entry = self.entries.get(cid_str)
+        if entry is not None:
+            entry.dec()
+            if entry.has_refs():  # don't delete if it has refs
+                return
+            else:
+                del self.entries[cid_str]
+                if self._stats is not None:
+                    self._stats.push(None, 'WantListSize', -1)  # TODO
+
+    def force_remove(self, cid: py_cid.cid):
+        cid_str = str(cid)
+        if self.entries.get(cid_str) is not None:
+            del self.entries[cid_str]
+
+    def __str__(self):
+        return f'WantList, number_of_entries={len(self.entries)}'
+
+    def __iter__(self):
+        yield from self.entries
+
+    def __contains__(self, item: py_cid.cid):
+        return str(item) in self.entries
+
+    def sorted_entries(self):
+        # TODO
+        raise NotImplementedError()
--- bitswap/types/wantlist/WantListEntry.py
+++ bitswap/types/wantlist/WantListEntry.py
@@ -2,18 +2,18 @@ import cid as py_cid
 
 
 class WantListEntry:
-    def __init__(self, cid:py_cid.cid, priority:int):
-        assert py_cid.is_cid(str(cid)), cid + "Is not a valid cid"
-        self._ref_counter:int = 1
+    def __init__(self, cid: py_cid.cid, priority: int = 1):
+        assert py_cid.is_cid(str(cid)), f"{cid} is not a valid cid"
+        self._ref_counter: int = 1
         self.cid = cid
-        self.priority = priority or True
+        self.priority = priority
 
     def inc(self):
         self._ref_counter += 1
 
     def dec(self):
-        minus_1 = self._ref_counter-1
-        self._ref_counter = 0 if 0>minus_1 else minus_1
+        minus_1 = self._ref_counter - 1
+        self._ref_counter = 0 if 0 > minus_1 else minus_1
 
     def has_refs(self):
         return self._ref_counter > 0
@@ -22,8 +22,8 @@ class WantListEntry:
         return f'WantListEntry(cid={str(self.cid)}, priority={self.priority}) refs:{self._ref_counter}'
 
     def __eq__(self, other):
-        return self.equals(other)
-
-    def equals(self, other):
         return self._ref_counter == other._ref_counter and str(self.cid) == str(
             other.cid) and self.priority == other.priority
+
+    def equals(self, other: 'WantListEntry'):
+        return self == other
